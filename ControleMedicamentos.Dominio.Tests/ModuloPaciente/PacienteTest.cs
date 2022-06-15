853a9c0c90112a379346ecc598d631aa64df80f4
lucasvs07 <78516282+lucasvs07@users.noreply.github.com>
2022-06-15 20:43:17 -0300

    Add project files.

---
+++ ControleMedicamentos.Dominio.Tests/ModuloPaciente/PacienteTest.cs
@@ -0,0 +1,44 @@
+﻿using ControleMedicamentos.Dominio.ModuloPaciente;
+using FluentValidation.Results;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleMedicamentos.Dominio.Tests.ModuloPaciente
+{
+    [TestClass]
+    public class PacienteTest
+    {
+        [TestMethod]
+        public void Caso_Nome_Nulo()
+        {
+            Paciente paciente = new(null, "256874598632145");
+
+            ValidadorPaciente pacienteTest = new();
+
+            ValidationResult resultado = pacienteTest.Validate(paciente);
+
+            Assert.AreEqual("Preencha o campo nome.", resultado.Errors[0].ErrorMessage);
+        }
+
+        [TestMethod]
+        public void Caso_Nome_Vazio()
+        {
+            //arrange
+            Paciente paciente = new("", "254785478965125");
+
+            ValidadorPaciente pacienteTest = new();
+
+            //action
+            ValidationResult resultado = pacienteTest.Validate(paciente);
+
+            //assert
+            Assert.AreEqual("Preencha o campo nome.", resultado.Errors[0].ErrorMessage);
+        }
+
+
+    }
+}
---
+++ ControleMedicamentos.Dominio/ModuloPaciente/ValidadorPaciente.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleMedicamentos.Dominio.ModuloPaciente
+{
+    public class ValidadorPaciente : AbstractValidator<Paciente>
+    {
+        public ValidadorPaciente()
+        {
+            RuleFor(x => x.Nome)
+                                .NotNull().WithMessage("Preencha o campo nome.")
+                                .NotEmpty().WithMessage("Preencha o campo nome.");
+
+            RuleFor(x => x.CartaoSUS)
+                                .NotNull().WithMessage("Preencha o número do cartão SUS.")
+                                .NotEmpty().WithMessage("Preencha o número do cartão SUS.")
+                                .MinimumLength(15).WithMessage("Cartão SUS deve ter 15 números.")
+                                .MaximumLength(15).WithMessage("Cartão SUS deve ter 15 números.");
+        }
+
+    }
+}
